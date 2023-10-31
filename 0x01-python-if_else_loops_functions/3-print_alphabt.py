aa5a8baf31a3672d281f634ead9d88be27689644
Samuel-ALX23 <ayitey_samuel@hotmail.com>
2023-10-31 16:35:34 +0000

    [Task 3] Print all the letters except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for lower_case in range(26):
-  if lower_case != 4 and lower_case != 16:
-    print("{}".format(chr(lower_case + ord("a"))), end="")
+for alphabet in range(26):
+    if alphabet != 4 and alphabet != 16:
+        print("{:s}".format(chr(alphabet + ord("a"))), end="")
