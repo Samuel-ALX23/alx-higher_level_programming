9c4c9431711d679ee22dfe97906607e365578c27
Samuel-ALX23 <ayitey_samuel@hotmail.com>
2023-11-05 23:03:53 +0000

    [Task all] Data Structure

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list or idx:
-        if idx < 0:
-            return my_list
-        elif idx >= len(my_list):
-            return my_list
-        else:
-            del my_list[idx]
-            new = 12-switch.pymy_list[:]
-            return new
-    else:
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
+    else:
+        del my_list[idx]
+    return my_list
